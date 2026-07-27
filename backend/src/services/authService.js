0d299db3a7d06d9249f6c2ff1a0fbe2559bf6455
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-07-27 09:21:39 +0000

    continuing

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -3,3 +3,12 @@ const {hashPassword,comparePassword}=require('../utils/Password')
 const {signAccessToken, signRefreshToken}=require('../utils/jwt')
 const AppError=require('../errors/AppError')
 
+const registerUser=async({name,email,password})=>{
+    const existingUser=await userRepository.findbyEmail(email)
+    if(!existingUser){
+        throw new AppError('Email already exists',409)
+    }
+
+    const passwordHash=await hashPassword(password)
+    const user=await userRepository.createUser({name,email,passwordHash})
+}
